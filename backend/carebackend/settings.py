67ec65c6ce72b88b984b61dfb454643ffe8cd7d5
Mike Krieger <m@mikeyk.co>
2020-03-21 18:14:44 -0700

    fixing logger to output for gunicorn

--- backend/carebackend/settings.py
+++ backend/carebackend/settings.py
@@ -120,6 +120,21 @@ USE_L10N = True
 
 USE_TZ = True
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'level': 'ERROR',
+            'handlers': ['console'],
+        },
+    }
+}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
