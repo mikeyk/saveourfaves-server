9e8079ebebb63eca04428298c7268a2e98f4c5a4
Mike Krieger <m@mikeyk.co>
2020-03-26 16:22:54 -0700

    Fixing blacklist checker for www prefixes

--- backend/places/helper.py
+++ backend/places/helper.py
@@ -4,6 +4,6 @@ from places.constants import BLACKLISTED_DOMAINS
 def check_link_against_blacklist(link):
     if link:
         parsed = urlparse(link)
-        if parsed.hostname.replace('www', '') in BLACKLISTED_DOMAINS:
+        if parsed.hostname.replace('www.', '') in BLACKLISTED_DOMAINS:
             return None
     return link
